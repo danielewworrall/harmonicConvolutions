f95c35ed10c514ea43e99af4cd8800c0c65866eb
StephanGarbin <stephan-garbin@hotmail.com>
2016-05-18 09:21:04 +0100

    better print, lr schedule

--- tensorflow/groupConvsBasicModel.py
+++ tensorflow/groupConvsBasicModel.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 import matplotlib.pyplot as pyPlot
 import numpy as np
 
-num_epochs = 4
+num_epochs = 200
 batch_size = 1024
-learning_rate = 1e-2 * 0.5
+learning_rate = 1e-2 * 0.15
 i_numRows = 28
 i_numCols = 28
 i_numChannels = 1
-alpha = 1000
+alpha = 10000
 fully_connected_size = 32
 
 TRAIN_FILE = 'data/MNIST/train.tfrecords'
@@ -72,6 +72,9 @@ Q = tf.Variable(tf.random_normal(shape=[9,9],mean=0.0, stddev=0.05), name="Q")
 #weight parameters of the convolution
 w = tf.Variable(tf.random_normal(shape=[3,3],mean=0.0, stddev=0.05), name="w")
 
+#lagrangian matrix
+lagrangianMatrix = tf.Variable(tf.random_normal(shape=[9,9],mean=0.0, stddev=0.05), name="lagrangianMultipliersQ")
+
 linearW0 = tf.Variable(tf.random_normal([i_numRows * i_numCols * i_numChannels, fully_connected_size]), name='linear_weight0')
 linearb0 = tf.Variable(tf.random_normal([fully_connected_size]), name='linear_bias0')
 
@@ -123,6 +126,7 @@ cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, label_bat
 
 with tf.name_scope('QConstraints') as scope:
 	Q_loss = tf.scalar_mul(alpha, tf.reduce_sum(tf.pow(tf.add(tf.matmul(tf.transpose(Q), Q), tf.neg(Q_identity)), 2)))
+	#Q_loss = tf.trace(tf.matmul(lagrangianMatrix, tf.add(tf.matmul(Q, tf.transpose(Q)), tf.neg(Q_identity))))
 
 loss = tf.add(Q_loss, tf.reduce_mean(cross_entropy))
 
@@ -140,9 +144,11 @@ with tf.name_scope('Accuracy') as scope:
 	accuracySummary = tf.scalar_summary("Accuracy", accuracy)
 
 #define optimiser
+graph_learning_rate = tf.placeholder(tf.float32, shape=[], name="learningRate")
 step = tf.Variable(0, name='global_step', trainable=False)
-optim = tf.train.AdamOptimizer(learning_rate)
+optim = tf.train.AdamOptimizer(graph_learning_rate)
 train_op = optim.minimize(loss, global_step=step)
+learningRate_summary = tf.scalar_summary("LearningRate", graph_learning_rate)
 
 #Graph construction, session etc. -------------------------------------------------
 sess = tf.Session()
@@ -166,10 +172,15 @@ loop_step = 0
 try:
 	while not coordinator.should_stop():
 		# Run training steps or whatever
-		result, currentLoss, summaryResult = sess.run([train_op, loss, summary_op], feed_dict={Q_identity: inputQ_identity})
+		result, currentLoss, currentAccuracy, summaryResult = sess.run([train_op, loss, accuracy, summary_op], feed_dict={Q_identity: inputQ_identity, graph_learning_rate: learning_rate})
 		summary.add_summary(summaryResult, loop_step)
-		print(currentLoss)
+		print(currentLoss, currentAccuracy)
 		loop_step += 1
+
+		if(loop_step > 0 and loop_step % 500 == 0):
+			print("Halving Learning Rate...")
+			learning_rate = learning_rate / 2
+
 		#or for a different session: graph_def=sess.graph_def
 except tf.errors.OutOfRangeError:
 	print('Done training -- epoch limit reached')
@@ -191,6 +202,8 @@ print(Q)
 print("Eigenvalues of Q:")
 Q_eig = np.linalg.eig(Q)
 print(Q_eig)
+
+numpy.save("Q.npy", Q)
 
 
 
