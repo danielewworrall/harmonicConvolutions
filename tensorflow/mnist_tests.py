4a6924e02d49a5ab7ac990e57d75c46ef7159875
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2016-09-23 16:01:05 +0100

    10 fitlers

--- tensorflow/mnist_tests.py
+++ tensorflow/mnist_tests.py
@@ -303,7 +303,7 @@ def run():
 	n_input = 784 # MNIST data input (img shape: 28*28)
 	n_classes = 10 # MNIST total classes (0-9 digits)
 	dropout = 0.75 # Dropout, probability to keep units
-	n_filters = 5
+	n_filters = 10
 	
 	# tf Graph input
 	x = tf.placeholder(tf.float32, [batch_size, n_input])
