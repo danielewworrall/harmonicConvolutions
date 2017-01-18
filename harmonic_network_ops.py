36e7cebb992a0eb2717dc4b824a0acf578c4bdaa
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-01-18 15:59:35 +0000

    rs_conv created

--- harmonic_network_ops.py
+++ harmonic_network_ops.py
@@ -20,11 +20,56 @@ def conv(X, W, strides, padding, name):
 
 def h_conv(X, W, strides=(1,1,1,1), padding='VALID', max_order=1, name='N'):
 	"""Inter-order (cross-stream) convolutions can be implemented as single
-	convolutions. For this we store data as 6D tensors and filters as 8D
+	convolution. For this we store data as 6D tensors and filters as 8D
 	tensors, at convolution, we reshape down to 4D tensors and expand again.
 	
 	X: tensor shape [mbatch,h,w,channels,complex,order]
 	Q: tensor dict---reshaped to [h,w,in,in.comp,in.ord,out,out.comp,out.ord]
+	P: tensor dict---phases
+	strides: as per tf convention (default (1,1,1,1))
+	padding: as per tf convention (default VALID)
+	filter_size: (default 3)
+	max_order: (default 1)
+	name: (default N)
+	"""
+	with tf.name_scope('hconv'+str(name)) as scope:
+		# Build data tensor
+		Xsh = X.get_shape().as_list()
+		X_ = tf.reshape(X, tf.concat(0,[Xsh[:3],[-1]]))
+		
+		#   Construct the stream-convolutions as one big filter
+		Q_ = []
+		for output_order in xrange(max_order+1):
+			# For each output order build input
+			Qr = []
+			Qi = []
+			for input_order in xrange(Xsh[3]):
+				fo = output_order - input_order
+				c = W[np.abs(fo)]
+				s = np.sign(fo)
+				# Choose a different filter depending on whether imput is real
+				if Xsh[4] == 2:
+					Qr += [c[0],-s*c[1]]
+					Qi += [c[1],s*c[0]]
+				else:
+					Qr += [c[0]]
+					Qi += [c[1]]
+			Q_ += [tf.concat(2, Qr), tf.concat(2, Qi)]
+		Q_ = tf.concat(3, Q_)
+		
+		R = conv(X_, Q_, strides, padding, name+'cconv')
+		Rsh = R.get_shape().as_list()
+		ns = tf.concat(0, [Rsh[:3],[max_order+1,2],[Rsh[3]/(2*(max_order+1))]])
+		return tf.reshape(R, ns)
+	
+
+def rs_conv(X, W, strides=(1,1,1,1), padding='VALID', max_order=1, name='N'):
+	"""Inter-order (cross-stream) convolutions can be implemented as single
+	convolution. For this we store data as 7D tensors and filters as 10D
+	tensors, at convolution, we reshape down to 4D tensors and expand again.
+	
+	X: tensor shape [mbatch,h,w,channel,group,r_order,s_order]
+	Q: tensor dict---reshaped to [h,w,in,in.comp,in.r_ord,in.s_ord,out,out.comp,out.r_ord,out.s_ord]
 	P: tensor dict---phases
 	strides: as per tf convention (default (1,1,1,1))
 	padding: as per tf convention (default VALID)
