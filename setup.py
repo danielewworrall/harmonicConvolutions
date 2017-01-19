8dec3d30503256a44663444765b632355af44f3a
Tess <blondegeek@gmail.com>
2017-01-18 16:51:16 -0800

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='harmonicConvolutions',
-      version='0',
-      url='http://github.com/deworrall92/harmonicConvolutions',
-      packages=['harmonicConvolutions'],
-      zip_safe=False)
