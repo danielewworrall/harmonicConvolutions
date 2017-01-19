452ebc1937d7072ac2deaee60fc97fba3d15ed32
Tess <blondegeek@gmail.com>
2017-01-18 16:50:43 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='harmonicConvolutions',
+      version='0',
+      url='http://github.com/deworrall92/harmonicConvolutions',
+      packages=['harmonicConvolutions'],
+      zip_safe=False)
