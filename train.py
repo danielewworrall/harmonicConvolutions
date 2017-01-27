ed929fa91df8ee5e1c4dde4eaac1388605240954
Stephan J Garbin <stephangarbin@outlook.com>
2017-01-27 15:37:17 +0000

    added fraction as optional terminal argument

--- train.py
+++ train.py
@@ -7,12 +7,13 @@ from model_assembly_train import build_all_and_train
 from settings import settings
 
 if __name__ == '__main__':
-	if len(sys.argv) != 5:
-		print('Please provide:')
+	if len(sys.argv) < 5:
+		print('Please provide at least:')
 		print('     -comma-separated list of device IDxs to use')
 		print('     -dataset name (rotated_mnist / cifar10)')
 		print('     -model name (as defined in harmonic_network_models.py)')
 		print('     -parent data directory')
+		print('     -fraction of training data to use')
 		sys.exit(1)
 	deviceIdxs = [int(x.strip()) for x in sys.argv[1].split(',')]
 	opt = {}
@@ -20,8 +21,9 @@ if __name__ == '__main__':
 	opt['dataset'] = sys.argv[2]
 	opt['model'] = sys.argv[3]
 	opt['data_dir'] = sys.argv[4]
-	opt['train_data_fraction'] = 0.01
-
+	if len(sys.argv) >= 6:
+		opt['train_data_fraction'] = float(sys.argv[5])
+	
 	#create configuration for different tests
 	options = settings(opt)
 	options.create_options()
