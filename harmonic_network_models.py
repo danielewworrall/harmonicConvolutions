15c650ac8fbc9d4f810412ae4f9b4b6153286f9e
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-02-01 14:43:27 +0000

    h_VGG with nl

--- harmonic_network_models.py
+++ harmonic_network_models.py
@@ -175,27 +175,37 @@ def h_VGG(opt, x, train_phase, device='/cpu:0'):
 	
 	# Block 1
 	res1_1 = hn_lite.conv2d(x, nf1, fs, max_order=mo, padding='SAME', name='1_1', device=d)
+	res1_1 = hn_lite.nonlinearity(res1_1, tf.nn.relu, name='n1_1', device=d)
 	res1_2 = hn_lite.conv2d(res1_1, nf1, fs, max_order=mo, padding='SAME', name='1_2', device=d)
+	res1_2 = hn_lite.nonlinearity(res1_2, tf.nn.relu, name='n1_2', device=d)
 	res1_mp = hn_lite.mean_pool(res1_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='1_mp')
 	
 	# Block 2
 	res2_1 = hn_lite.conv2d(res1_mp, nf2, fs, max_order=mo, padding='SAME', name='2_1', device=d)
+	res2_1 = hn_lite.nonlinearity(res2_1, tf.nn.relu, name='n2_1', device=d)
 	res2_2 = hn_lite.conv2d(res2_1, nf2, fs, max_order=mo, padding='SAME', name='2_2', device=d)
+	res2_2 = hn_lite.nonlinearity(res2_2, tf.nn.relu, name='n2_2', device=d)
 	res2_mp = hn_lite.mean_pool(res2_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='2_mp')
 	
 	# Block 3
 	res3_1 = hn_lite.conv2d(res2_mp, nf3, fs, max_order=mo, padding='SAME', name='3_1', device=d)
+	res3_1 = hn_lite.nonlinearity(res3_1, tf.nn.relu, name='n3_1', device=d)
 	res3_2 = hn_lite.conv2d(res3_1, nf3, fs, max_order=mo, padding='SAME', name='3_2', device=d)
+	res3_2 = hn_lite.nonlinearity(res3_2, tf.nn.relu, name='n3_2', device=d)
 	res3_mp = hn_lite.mean_pool(res3_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='3_mp')
 	
 	# Block 4
 	res4_1 = hn_lite.conv2d(res3_mp, nf4, fs, max_order=mo, padding='SAME', name='4_1', device=d)
+	res4_1 = hn_lite.nonlinearity(res4_1, tf.nn.relu, name='n4_1', device=d)
 	res4_2 = hn_lite.conv2d(res4_1, nf4, fs, max_order=mo, padding='SAME', name='4_2', device=d)
+	res4_2 = hn_lite.nonlinearity(res4_2, tf.nn.relu, name='n4_2', device=d)
 	res4_mp = hn_lite.mean_pool(res4_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='4_mp')
 	
 	# Block 5
 	res5_1 = hn_lite.conv2d(res4_mp, nf5, fs, max_order=mo, padding='SAME', name='5_1', device=d)
+	res5_1 = hn_lite.nonlinearity(res5_1, tf.nn.relu, name='n5_1', device=d)
 	res5_2 = hn_lite.conv2d(res5_1, nf5, fs, max_order=mo, padding='SAME', name='5_2', device=d)
+	res5_2 = hn_lite.nonlinearity(res5_2, tf.nn.relu, name='n5_2', device=d)
 	res5_mp = hn_lite.mean_pool(res5_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='5_mp')
 	
 
