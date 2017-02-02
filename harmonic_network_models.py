510c94c96a48c4056f41ff097817b3795e32db7b
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-02-02 11:12:45 +0000

    smaller filters and log batch norm

--- harmonic_network_models.py
+++ harmonic_network_models.py
@@ -174,46 +174,47 @@ def h_VGG(opt, x, train_phase, device='/cpu:0'):
 		x = tf.reshape(x, shape=[bs,opt['dim'],opt['dim'],1,1,opt['n_channels']])
 	
 	activations = []
+	bn = hn_lite.log_batch_norm
 	
 	# Block 1
 	res1_1 = hn_lite.conv2d(x, nf1, fs, max_order=mo, n_rings=nr, padding='SAME', name='1_1', device=d)
-	res1_1 = hn_lite.batch_norm(res1_1, tp, tf.nn.relu, name='n1_1', device=d)
+	res1_1 = bn(res1_1, tp, tf.nn.relu, name='n1_1', device=d)
 	res1_2 = hn_lite.conv2d(res1_1, nf1, fs, max_order=mo, n_rings=nr, padding='SAME', name='1_2', device=d)
-	res1_2 = hn_lite.batch_norm(res1_2, tp, fnc=tf.nn.relu, name='n1_2', device=d)
+	res1_2 = bn(res1_2, tp, fnc=tf.nn.relu, name='n1_2', device=d)
 	res1_mp = hn_lite.mean_pool(res1_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='1_mp')
 	
 	# Block 2
 	res2_1 = hn_lite.conv2d(res1_mp, nf2, fs, max_order=mo, n_rings=nr, padding='SAME', name='2_1', device=d)
-	res2_1 = hn_lite.batch_norm(res2_1, tp, tf.nn.relu, name='n2_1', device=d)
+	res2_1 = bn(res2_1, tp, tf.nn.relu, name='n2_1', device=d)
 	res2_2 = hn_lite.conv2d(res2_1, nf2, fs, max_order=mo, n_rings=nr, padding='SAME', name='2_2', device=d)
-	res2_2 = hn_lite.batch_norm(res2_2, tp, tf.nn.relu, name='n2_2', device=d)
+	res2_2 = bn(res2_2, tp, tf.nn.relu, name='n2_2', device=d)
 	res2_mp = hn_lite.mean_pool(res2_2, ksize=(1,2,2,1), strides=(1,2,2,1), name='2_mp')
 	
 	# Block 3
 	res3_1 = hn_lite.conv2d(res2_mp, nf3, fs, max_order=mo, n_rings=nr, padding='SAME', name='3_1', device=d)
-	res3_1 = hn_lite.batch_norm(res3_1, tp, tf.nn.relu, name='n3_1', device=d)
+	res3_1 = bn(res3_1, tp, tf.nn.relu, name='n3_1', device=d)
 	res3_2 = hn_lite.conv2d(res3_1, nf3, fs, max_order=mo, n_rings=nr, padding='SAME', name='3_2', device=d)
-	res3_2 = hn_lite.batch_norm(res3_2, tp, tf.nn.relu, name='n3_2', device=d)
+	res3_2 = bn(res3_2, tp, tf.nn.relu, name='n3_2', device=d)
 	res3_3 = hn_lite.conv2d(res3_2, nf3, fs, max_order=mo, n_rings=nr, padding='SAME', name='3_3', device=d)
-	res3_3 = hn_lite.batch_norm(res3_3, tp, tf.nn.relu, name='n3_3', device=d)
+	res3_3 = bn(res3_3, tp, tf.nn.relu, name='n3_3', device=d)
 	res3_mp = hn_lite.mean_pool(res3_3, ksize=(1,2,2,1), strides=(1,2,2,1), name='3_mp')
 	
 	# Block 4
 	res4_1 = hn_lite.conv2d(res3_mp, nf4, fs, max_order=mo, n_rings=nr, padding='SAME', name='4_1', device=d)
-	res4_1 = hn_lite.batch_norm(res4_1, tp, tf.nn.relu, name='n4_1', device=d)
+	res4_1 = bn(res4_1, tp, tf.nn.relu, name='n4_1', device=d)
 	res4_2 = hn_lite.conv2d(res4_1, nf4, fs, max_order=mo, n_rings=nr, padding='SAME', name='4_2', device=d)
-	res4_2 = hn_lite.batch_norm(res4_2, tp, tf.nn.relu, name='n4_2', device=d)
+	res4_2 = bn(res4_2, tp, tf.nn.relu, name='n4_2', device=d)
 	res4_3 = hn_lite.conv2d(res4_2, nf4, fs, max_order=mo, n_rings=nr, padding='SAME', name='4_3', device=d)
-	res4_3 = hn_lite.batch_norm(res4_3, tp, tf.nn.relu, name='n4_3', device=d)
+	res4_3 = bn(res4_3, tp, tf.nn.relu, name='n4_3', device=d)
 	res4_mp = hn_lite.mean_pool(res4_3, ksize=(1,2,2,1), strides=(1,2,2,1), name='4_mp')
 	
 	# Block 5
 	res5_1 = hn_lite.conv2d(res4_mp, nf5, fs, max_order=mo, n_rings=nr, padding='SAME', name='5_1', device=d)
-	res5_1 = hn_lite.batch_norm(res5_1, tp, tf.nn.relu, name='n5_1', device=d)
+	res5_1 = bn(res5_1, tp, tf.nn.relu, name='n5_1', device=d)
 	res5_2 = hn_lite.conv2d(res5_1, nf5, fs, max_order=mo, n_rings=nr, padding='SAME', name='5_2', device=d)
-	res5_2 = hn_lite.batch_norm(res5_2, tp, tf.nn.relu, name='n5_2', device=d)
+	res5_2 = bn(res5_2, tp, tf.nn.relu, name='n5_2', device=d)
 	res5_3 = hn_lite.conv2d(res5_2, nf5, fs, max_order=mo, n_rings=nr, padding='SAME', name='5_3', device=d)
-	res5_3 = hn_lite.batch_norm(res5_3, tp, tf.nn.relu, name='n5_3', device=d)
+	res5_3 = bn(res5_3, tp, tf.nn.relu, name='n5_3', device=d)
 	res5_mp = hn_lite.mean_pool(res5_3, ksize=(1,2,2,1), strides=(1,2,2,1), name='5_mp')
 	
 
