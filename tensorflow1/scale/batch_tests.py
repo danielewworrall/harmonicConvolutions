8fbd460eff99cb94688aff72aa881ea188175c4f
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-02-26 13:25:37 +0000

    batch_tests updated to smaller equivariance weights

--- tensorflow1/scale/batch_tests.py
+++ tensorflow1/scale/batch_tests.py
@@ -13,17 +13,17 @@ from matplotlib import pyplot as plt
 
 def main():
 	opt = {}
-	opt['equivariant_weight'] = 1e-1
+	opt['equivariant_weight'] = 1e-6
 	accs = []
 	N_trials = 10
-	'''
+	
 	for i in xrange(N_trials):
 		acc = se.main(opt)
 		accs.append(acc)
-		np.save('./batch_tests/equi_1e_n1.npy', accs)
+		np.save('./batch_tests/equi_{:.0e}.npy'.format(opt['equivariant_weight']), accs)
 	'''
 	plt.figure(1)
-	names = ['1e_n1.npy','1e_n2.npy','1e_n3.npy','0.npy',]
+	names = ['1e-1.npy','1e-2.npy','1e-3.npy','0.npy',]
 	equi = [1e-1, 1e-2, 1e-3, 0.]
 	
 	means = []
@@ -36,7 +36,7 @@ def main():
 	print equi, means
 	plt.plot(equi, means)
 	plt.show()
-
+	'''
 
 if __name__ == '__main__':
 	main()
