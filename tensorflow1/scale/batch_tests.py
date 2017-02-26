8d578f9fb9fcac9ec2c9cf474bd8a615049aa503
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-02-26 13:13:53 +0000

    batch_tests complete, now plot

--- tensorflow1/scale/batch_tests.py
+++ tensorflow1/scale/batch_tests.py
@@ -8,25 +8,30 @@ import numpy as np
 
 import sgd_equivariance as se
 
+from matplotlib import pyplot as plt
+
 
 def main():
 	opt = {}
-	opt['equivariant_weight'] = 1e-3
+	opt['equivariant_weight'] = 1e-1
 	accs = []
 	N_trials = 10
 	'''
 	for i in xrange(N_trials):
 		acc = se.main(opt)
 		accs.append(acc)
-		np.save('./batch_tests/equi_1e_n3b.npy', accs)
+		np.save('./batch_tests/equi_1e_n1.npy', accs)
 	'''
-	data0 = np.load('./batch_tests/equi_0b.npy')
-	data3 = np.load('./batch_tests/equi_1e_n3b.npy')
-	print data0
-	print('Plain: {:04f}, {:04f}'.format(np.mean(data0), np.std(data0)))
-	print data3
-	print('Equi: {:04f}, {:04f}'.format(np.mean(data3), np.std(data3)))
-	
+	plt.figure(1)
+	names = ['1e_n1.npy','1e_n2.npy','1e_n3.npy','0.npy',]
+	equi = [1e-1, 1e-2, 1e-3, 0.]
+	for i, name in enumerate(['1e_n1.npy','1e_n2.npy','1e_n3.npy','0.npy',]):
+		fname = './batch_tests/equi_' + name
+		data = np.load(fname)
+		print data
+		print('{:s}: {:04f}, {:04f}'.format(fname, np.mean(data), np.std(data)))
+		plt.plot(equi[i], np.mean(data))
+	plt.show()
 
 
 if __name__ == '__main__':
