1c674401f0c6f145aaa48606da1fc9777565d84a
Daniyar Turmukhambetov <dant.kz@gmail.com>
2017-03-12 21:42:02 +0000

    .

--- tensorflow1/scale/autoencoder_shapenet.py
+++ tensorflow1/scale/autoencoder_shapenet.py
@@ -20,6 +20,29 @@ from spatial_transformer import transformer
 
 ###   TMP   ###
 
+# x-axis-rot, y-axis-rot, z-axis-rot
+def transmat(phi, theta, psi, shiftmat=None):
+    batch_size = phi.shape[0]
+    assert batch_size==theta.shape[0] and batch_size==psi.shape[0], 'must have same number of angles for x,y and z axii'
+    assert phi.ndim==1 and theta.ndim==1 and psi.ndim==1, 'must be 1 dimensional array'
+
+    if shiftmat is None:
+        shiftmat = np.zeros([batch_size,3,1])
+
+    rotmat = np.zeros([batch_size, 3,3])
+    rotmat[:,0,0] = np.cos(theta)*np.cos(psi)
+    rotmat[:,0,1] = np.cos(phi)*np.sin(psi) + np.sin(phi)*np.sin(theta)*np.cos(psi)
+    rotmat[:,0,2] = np.sin(phi)*np.sin(psi) - np.cos(phi)*np.sin(theta)*np.cos(psi)
+    rotmat[:,1,0] = -np.cos(theta)*np.sin(psi)
+    rotmat[:,1,1] = np.cos(phi)*np.cos(psi) - np.sin(phi)*np.sin(theta)*np.sin(psi)
+    rotmat[:,1,2] = np.sin(phi)*np.cos(psi) + np.cos(phi)*np.sin(theta)*np.sin(psi)
+    rotmat[:,2,0] = np.sin(theta)
+    rotmat[:,2,1] = -np.sin(phi)*np.cos(theta)
+    rotmat[:,2,2] = np.cos(phi)*np.cos(theta)
+
+    transmat = np.concatenate([rotmat, shiftmat],2)
+    return np.reshape(transmat, [batch_size, -1]).astype(np.float32)
+
 pad_size = 12
 vol = np.pad(vol, pad_width=[[pad_size,pad_size], [pad_size,pad_size], [pad_size,pad_size]], mode='constant')
 outsize = (int(vol.shape[0]), int(vol.shape[1]), int(vol.shape[2]))
