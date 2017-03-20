82963cd5d77c499703634a17d7d77c82ce13d386
Daniyar <daniyar.turmukhambetov.10@ucl.ac.uk>
2017-03-20 19:26:27 +0000

    i think i fixed memory leak. don't grow your computational graph in a for loop, kids.

--- tensorflow1/scale/autoencoder_shapenet.py
+++ tensorflow1/scale/autoencoder_shapenet.py
@@ -11,6 +11,7 @@ import scipy.misc
 
 ### local files ######
 
+from vol_util import *
 import shapenet_loader
 import modelnet_loader
 import equivariant_loss as el
@@ -33,67 +34,6 @@ flags.DEFINE_boolean('Sleepy', False, 'Sleepy execution environment')
 flags.DEFINE_boolean('Dopey', True, 'Dopey execution environment')
 flags.DEFINE_boolean('DaniyarSleepy', False, 'Dopey execution environment')
 ##---------------------
-
-################ UTIL #################
-def tf_im_summary(prefix, images):
-    for j in xrange(min(10, images.get_shape().as_list()[0])):
-        desc_str = '%d'%(j) + '_' + prefix
-        tf.summary.image(desc_str, images[j:(j+1), :, :, :], max_outputs=1)
-
-def tf_vol_summary(prefix, vols):
-    # need to keep 4-dim tensor for tf_im_summary
-    #vols = tf.reduce_sum(vols, axis=4)
-
-    vols_d = tf.reduce_sum(vols, axis=1)
-    vols_h = tf.reduce_sum(vols, axis=2)
-    vols_w = tf.reduce_sum(vols, axis=3)
-
-    vols_d = vols_d / tf.reduce_max(vols_d, axis=[1,2,3], keep_dims=True)
-    vols_h = vols_h / tf.reduce_max(vols_h, axis=[1,2,3], keep_dims=True)
-    vols_w = vols_w / tf.reduce_max(vols_w, axis=[1,2,3], keep_dims=True)
-
-    tf_im_summary(prefix + '_d', vols_d)
-    tf_im_summary(prefix + '_h', vols_h)
-    tf_im_summary(prefix + '_w', vols_w)
-
-
-def imsave(path, img):
-    if img.shape[-1]==1:
-        img = np.squeeze(img)
-    scipy.misc.toimage(img, cmin=0.0, cmax=1.0).save(path)
-
-def get_imgs_from_vol(tile_image, tile_h, tile_w):
-    tile_image = tile_image.astype(np.float32)
-    tile_image = np.sum(tile_image, axis=4)
-    tile_image_d = np.sum(tile_image, axis=1)
-    tile_image_h = np.sum(tile_image, axis=2)
-    tile_image_w = np.sum(tile_image, axis=3)
-
-    d_sum = np.sum(np.sum(tile_image_d, axis=2, keepdims=True), axis=1, keepdims=True)
-    h_sum = np.sum(np.sum(tile_image_h, axis=2, keepdims=True), axis=1, keepdims=True)
-    w_sum = np.sum(np.sum(tile_image_w, axis=2, keepdims=True), axis=1, keepdims=True)
-
-    d_max = tile_image_d.max(axis=2, keepdims=True).max(axis=1, keepdims=True)
-    h_max = tile_image_h.max(axis=2, keepdims=True).max(axis=1, keepdims=True)
-    w_max = tile_image_w.max(axis=2, keepdims=True).max(axis=1, keepdims=True)
-    
-    tile_image_d = tile_image_d / d_max
-    tile_image_h = tile_image_h / h_max
-    tile_image_w = tile_image_w / w_max
-
-    def tile_batch(batch, tile_w=1, tile_h=1):
-        assert tile_w * tile_h == batch.shape[0], 'tile dimensions inconsistent'
-        batch = np.split(batch, tile_w*tile_h, axis=0)
-        batch = [np.concatenate(batch[i*tile_w:(i+1)*tile_w], 2) for i in range(tile_h)]
-        batch = np.concatenate(batch, 1)
-        batch = batch[0,:,:]
-        return batch
-
-    tile_image_d = tile_batch(tile_image_d, tile_h, tile_w)
-    tile_image_h = tile_batch(tile_image_h, tile_h, tile_w)
-    tile_image_w = tile_batch(tile_image_w, tile_h, tile_w)
-
-    return tile_image_d, tile_image_h, tile_image_w
 
 ################ DATA #################
 
@@ -141,136 +81,34 @@ def autoencoder(x, num_latents, f_params, is_training, reuse=False):
     return recons, codes, recons_logits
 
 
-def variable(name, shape=None, initializer=tf.contrib.layers.xavier_initializer_conv2d(uniform=False), trainable=True):
-    #tf.random_normal_initializer(stddev=0.0001)
-    #tf.constant_initializer(0.0)
-    with tf.device('/gpu:0'):
-        var = tf.get_variable(name, shape, initializer=initializer, trainable=trainable)
-    return var
-
-def tf_nn_lrelu(x, alpha=0.1, name='lrelu'):
-    return tf.nn.relu(x, name=name+'_a') + tf.nn.relu(-alpha*x, name=name+'_b')
-
 def encoder(x, num_latents, is_training, reuse=False):
     """Encoder with conv3d"""
     
-    def convlayer(i, inp, ksize, inpdim, outdim, stride, reuse, nonlin=tf.nn.elu, dobn=True, padding='SAME'):
-        scopename = 'conv_layer' + str(i)
-        print(scopename)
-        print(' input:', inp)
-        strides = [1, stride, stride, stride, 1]
-        with tf.variable_scope(scopename) as scope:
-            if reuse:
-                scope.reuse_variables()
-            kernel = variable(scopename + '_kernel', [ksize, ksize, ksize, inpdim, outdim])
-            bias = variable(scopename + '_bias', [outdim], tf.constant_initializer(0.0))
-            linout = tf.nn.conv3d(inp, kernel, strides=strides, padding=padding)
-            linout = tf.nn.bias_add(linout, bias)
-            if dobn:
-                bnout = bn5d(linout, is_training, reuse=reuse)
-            else:
-                bnout = linout
-            out = nonlin(bnout, name=scopename + '_nonlin')
-        print(' out:', out)
-        return out
-
-    l0 = convlayer(0, x,  3, 1,      8,  1, reuse)                   #  48-46
-    l1 = convlayer(1, l0, 3, 8,     16,  2, reuse)                   #  46-24
-    l2 = convlayer(2, l1, 3, 16,    16,  1, reuse)                  #  24-24
-    l3 = convlayer(3, l2, 3, 16,    32,  2, reuse)                   #  24-12
-    l4 = convlayer(4, l3, 3, 32,    32,  1, reuse)                  #  12-12
-    l5 = convlayer(5, l4, 3, 32,    64,  2, reuse)                   #  12-6
-    l6 = convlayer(6, l5, 3, 64,    64,  1, reuse)                   #  6-6
-    l7 = convlayer(7, l6, 6, 64,   512,  1, reuse, padding='VALID')  #  6-1 
-    codes = convlayer(8, l7, 1, 512, num_latents, 1, reuse, nonlin=tf.identity, dobn=False) # 1 -> 1
+    l0 = convlayer(0, x,  3, 1,      8,  1, reuse, is_training=is_training)                   #  48-46
+    l1 = convlayer(1, l0, 3, 8,     16,  2, reuse, is_training=is_training)                   #  46-24
+    l2 = convlayer(2, l1, 3, 16,    16,  1, reuse, is_training=is_training)                  #  24-24
+    l3 = convlayer(3, l2, 3, 16,    32,  2, reuse, is_training=is_training)                   #  24-12
+    l4 = convlayer(4, l3, 3, 32,    32,  1, reuse, is_training=is_training)                  #  12-12
+    l5 = convlayer(5, l4, 3, 32,    64,  2, reuse, is_training=is_training)                   #  12-6
+    l6 = convlayer(6, l5, 3, 64,    64,  1, reuse, is_training=is_training)                   #  6-6
+    l7 = convlayer(7, l6, 6, 64,   512,  1, reuse, is_training=is_training, padding='VALID')  #  6-1 
+    codes = convlayer(8, l7, 1, 512, num_latents, 1, reuse, nonlin=tf.identity, is_training=is_training, dobn=False) # 1 -> 1
     return codes
-
-
-def vol_resize_nearest(x, outshapes, align_corners=None):
-    outdepth, outheight, outwidth = outshapes
-    batch_size, depth, height, width, inpdim = x.get_shape().as_list()
-    xd_hw = tf.reshape(x, [-1, height, width, inpdim])
-    xd_hhww = tf.image.resize_nearest_neighbor(xd_hw, [outheight, outwidth], align_corners=align_corners)
-    x_dhhww = tf.reshape(xd_hhww, [batch_size, depth, outheight, outwidth, inpdim])
-    x_hhwwd = tf.transpose(x_dhhww, [0,2,3,1,4])
-    xhh_wwd = tf.reshape(x_hhwwd, [-1, outwidth, depth, inpdim])
-    xhh_wwdd = tf.image.resize_nearest_neighbor(xhh_wwd, [outwidth, outdepth], align_corners=align_corners)
-    x_hhwwdd = tf.reshape(xhh_wwdd, [batch_size, outheight, outwidth, outdepth, inpdim])
-    x_ddhhww = tf.transpose(x_hhwwdd, [0,3,1,2,4])
-    return x_ddhhww
 
 
 def decoder(codes, is_training, reuse=False):
     num_latents = codes.get_shape()[-1]
 
-    def upconvlayer(i, inp, ksize, inpdim, outdim, outshape, reuse, nonlin=tf.nn.elu, dobn=True):
-        scopename = 'upconv_layer' + str(i)
-        inpshape = inp.get_shape().as_list()[-2]
-        print(scopename)
-        print(' input:', inp)
-        #pad_size = int(ksize//2)
-        #paddings = [[0,0], [pad_size, pad_size], [pad_size, pad_size], [pad_size, pad_size], [0,0]]
-        output_shape = [outshape, outshape, outshape]
-        padding='SAME'
-        with tf.variable_scope(scopename) as scope:
-            if reuse:
-                scope.reuse_variables()
-            kernel = variable(scopename + '_kernel', [ksize, ksize, ksize, inpdim, outdim])
-            bias = variable(scopename + '_bias', [outdim], tf.constant_initializer(0.0))
-            if outshape>inpshape:
-                inp_resized = vol_resize_nearest(inp, output_shape)
-            else:
-                inp_resized = inp
-            #inp_resized_padded = tf.pad(inp_resized, paddings, mode='SYMMETRIC')
-            linout = tf.nn.conv3d(inp_resized, kernel, strides=[1, 1, 1, 1, 1], padding=padding)
-            linout = tf.nn.bias_add(linout, bias)
-            if dobn:
-                bnout = bn5d(linout, is_training, reuse=reuse)
-            else:
-                bnout = linout
-            out = nonlin(bnout, name=scopename + 'nonlin')
-        print(' out:', out)
-        return out
 
-    def upconvlayer_tr(i, inp, ksize, inpdim, outdim, outshape, stride, reuse, nonlin=tf.nn.elu, dobn=True, padding='SAME'):
-        scopename = 'upconv_layer_tr_' + str(i)
-        print(scopename)
-        print(' input:', inp)
-        output_shape = [inp.get_shape().as_list()[0], outshape, outshape, outshape, outdim]
-        strides = [1, stride, stride, stride, 1]
-        with tf.variable_scope(scopename) as scope:
-            if reuse:
-                scope.reuse_variables()
-            kernel = variable(scopename + '_kernel', [ksize, ksize, ksize, outdim, inpdim])
-            bias = variable(scopename + '_bias', [outdim], tf.constant_initializer(0.0))
-            linout = tf.nn.conv3d_transpose(inp, kernel, output_shape, strides=strides, padding=padding)
-            linout = tf.nn.bias_add(linout, bias)
-            if dobn:
-                bnout = bn5d(linout, is_training, reuse=reuse)
-            else:
-                bnout = linout
-            out = nonlin(bnout, name=scopename + 'nonlin')
-        print(' out:', out)
-        return out
-
-    #l0 = upconvlayer(0,     codes, 1, num_latents, 512, 1, 1, reuse)
-    #l1 = upconvlayer(1,     codes, 1, num_latents, 2048, 1, 1, reuse) #  1 -> 2
-    #l2 = upconvlayer(2,     l1,    4, 2048,        512,  4, 4, reuse) #  2 -> 4
-    #l3 = upconvlayer(3,     l2,    4, 512,         256,  7, 2, reuse) #  4 -> 7
-    #l4 = upconvlayer(4,     l3,    5, 256,         128, 14, 2, reuse) #  7 -> 14
-    #l5 = upconvlayer(5,     l4,    5, 128,         64,  28, 2, reuse) # 14 -> 28
-    #l6 = upconvlayer(6,     l5,    5, 64,          32,  56, 2, reuse) # 28 -> 56
-    #recons = upconvlayer(7, l6,    3, 32,          1,   56, 1, reuse, nonlin=tf.nn.sigmoid)
-
-    l1 = upconvlayer(1,             codes, 1, num_latents, 512,   1, reuse) 
-    l2 = upconvlayer_tr(2,          l1,    6, 512,         64,    6, 6, reuse)
-    l3 = upconvlayer(3,             l2,    3, 64,          64,    6, reuse)
-    l4 = upconvlayer(4,             l3,    3, 64,          64,   12, reuse)
-    l5 = upconvlayer(5,             l4,    3, 64,          32,   12, reuse)
-    l6 = upconvlayer(6,             l5,    3, 32,          32,   24, reuse)
-    l7 = upconvlayer(7,             l6,    3, 32,          16,   24, reuse)
-    l8 = upconvlayer(8,             l7,    3, 16,          16,   48, reuse)
-    recons_logits = upconvlayer(9,  l8,    3, 16,          1,    48, reuse, nonlin=tf.identity, dobn=False)
+    l1 = upconvlayer(1,             codes, 1, num_latents, 512,   1, reuse, is_training=is_training) 
+    l2 = upconvlayer_tr(2,          l1,    6, 512,         64,    6, 6, reuse, is_training=is_training)
+    l3 = upconvlayer(3,             l2,    3, 64,          64,    6, reuse, is_training=is_training)
+    l4 = upconvlayer(4,             l3,    3, 64,          64,   12, reuse, is_training=is_training)
+    l5 = upconvlayer(5,             l4,    3, 64,          32,   12, reuse, is_training=is_training)
+    l6 = upconvlayer(6,             l5,    3, 32,          32,   24, reuse, is_training=is_training)
+    l7 = upconvlayer(7,             l6,    3, 32,          16,   24, reuse, is_training=is_training)
+    l8 = upconvlayer(8,             l7,    3, 16,          16,   48, reuse, is_training=is_training)
+    recons_logits = upconvlayer(9,  l8,    3, 16,          1,    48, reuse, is_training=is_training, nonlin=tf.identity, dobn=False)
     recons = tf.sigmoid(recons_logits)
     return recons, recons_logits
 
@@ -315,63 +153,7 @@ def bernoulli_xentropy(target, output):
     output = 0.8999*output + 0.1000
     wx_entropy = -(98.0 * target * tf.log(output) + 2.0*(1.0 - target) * tf.log(1.0 - output))/100.0
     return (tf.reduce_sum(wx_entropy, axis=(1,2,3,4)))
-    #diff = tf.square(target-output)
-    #return tf.reduce_mean(tf.reduce_sum(diff, axis=(1,2,3,4)))
 
-def spatial_transform(stl, x, transmat, paddings):
-    x_padded = tf.pad(x, paddings, mode='CONSTANT')
-    batch_size = transmat.get_shape().as_list()[0]
-    shiftmat = tf.zeros([batch_size,3,1], dtype=tf.float32)
-    transmat_full = tf.concat([transmat, shiftmat], axis=2)
-    transmat_full = tf.reshape(transmat_full, [batch_size, -1])
-    x_in = stl.transform(x_padded, transmat_full)
-    # thresholding
-    x_in = tf.floor(0.5 + x_in)
-    return x_in
-
-def get_3drotmat(xyzrot):
-    assert xyzrot.ndim==2, 'xyzrot must be 2 dimensional array'
-    batch_size = xyzrot.shape[0]
-    assert xyzrot.shape[1]==3, 'must have rotation angles for x,y and z axii'
-    phi = xyzrot[:,0]
-    theta = xyzrot[:,1]
-    psi = xyzrot[:,2]
-    rotmat = np.zeros([batch_size, 3, 3])
-    rotmat[:,0,0] = np.cos(theta)*np.cos(psi)
-    rotmat[:,0,1] = np.cos(phi)*np.sin(psi) + np.sin(phi)*np.sin(theta)*np.cos(psi)
-    rotmat[:,0,2] = np.sin(phi)*np.sin(psi) - np.cos(phi)*np.sin(theta)*np.cos(psi)
-    rotmat[:,1,0] = -np.cos(theta)*np.sin(psi)
-    rotmat[:,1,1] = np.cos(phi)*np.cos(psi) - np.sin(phi)*np.sin(theta)*np.sin(psi)
-    rotmat[:,1,2] = np.sin(phi)*np.cos(psi) + np.cos(phi)*np.sin(theta)*np.sin(psi)
-    rotmat[:,2,0] = np.sin(theta)
-    rotmat[:,2,1] = -np.sin(phi)*np.cos(theta)
-    rotmat[:,2,2] = np.cos(phi)*np.cos(theta)
-    return rotmat
-
-
-def get_3dscalemat(xyzfactor):
-    batch_size = xyzfactor.shape[0]
-    assert xyzfactor.ndim==2, 'xyzfactor must be a 2 dimensional array'
-    assert xyzfactor.shape[1]==3, 'xyzfactor must have scale factor for each axis'
-    scalemat = np.zeros([batch_size, 3, 3])
-    scalemat[:,0,0] = xyzfactor[:,0]
-    scalemat[:,1,1] = xyzfactor[:,1]
-    scalemat[:,2,2] = xyzfactor[:,2]
-    return scalemat
-
-def get_2drotscalemat(theta, min_scale, max_scale):
-    batch_size = theta.shape[0]
-    Rot = np.zeros([batch_size, 2, 2])
-
-    if max_scale>min_scale + 1e-6:
-        theta = theta-min_scale
-        theta = theta/(max_scale-min_scale)
-    theta = np.pi*theta
-    Rot[:,0,0] = np.cos(theta)
-    Rot[:,0,1] = -np.sin(theta)
-    Rot[:,1,0] = np.sin(theta)
-    Rot[:,1,1] = np.cos(theta)
-    return Rot
 
 def random_transmats(batch_size):
     """ Random rotations in 3D
@@ -453,39 +235,6 @@ def update_f_params(f_params, rot_or_scale, ax, theta):
         inp_f_2dscalemat = get_2drotscalemat(angles, 1.0, 1.0)
         f_params = set_f_params_scale(f_params, ax, inp_f_2dscalemat)
     return f_params
-
-
-##### SPECIAL FUNCTIONS #####
-def bn5d(X, train_phase, decay=0.99, name='batchNorm', reuse=False):
-    assert len(X.get_shape().as_list())==5, 'input to bn5d must be 5d tensor'
-
-    n_out = X.get_shape().as_list()[-1]
-    
-    beta = tf.get_variable('beta_'+name, dtype=tf.float32, shape=n_out, initializer=tf.constant_initializer(0.0))
-    gamma = tf.get_variable('gamma_'+name, dtype=tf.float32, shape=n_out, initializer=tf.constant_initializer(1.0))
-    pop_mean = tf.get_variable('pop_mean_'+name, dtype=tf.float32, shape=n_out, trainable=False)
-    pop_var = tf.get_variable('pop_var_'+name, dtype=tf.float32, shape=n_out, trainable=False)
-
-    batch_mean, batch_var = tf.nn.moments(X, [0, 1, 2, 3], name='moments_'+name)
-    
-    if not reuse:
-    	ema = tf.train.ExponentialMovingAverage(decay=decay)
-    
-    	def mean_var_with_update():
-    		ema_apply_op = ema.apply([batch_mean, batch_var])
-    		pop_mean_op = tf.assign(pop_mean, ema.average(batch_mean))
-    		pop_var_op = tf.assign(pop_var, ema.average(batch_var))
-    
-    		with tf.control_dependencies([ema_apply_op, pop_mean_op, pop_var_op]):
-    			return tf.identity(batch_mean), tf.identity(batch_var)
-    	
-    	mean, var = tf.cond(train_phase, mean_var_with_update,
-    				lambda: (pop_mean, pop_var))
-    else:
-    	mean, var = tf.cond(train_phase, lambda: (batch_mean, batch_var),
-    			lambda: (pop_mean, pop_var))
-    	
-    return tf.nn.batch_normalization(X, mean, var, beta, gamma, 1e-5)
 
 
 ############################################
